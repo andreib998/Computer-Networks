7adfd058605fa328aa075f3ffb42df60ce49d7bf
Alexandru Darie <alexdarieph@gmail.com>
2018-11-04 17:06:47 +0200

    Update client.c

--- simple-tcp-ip-app-C/client.c
+++ simple-tcp-ip-app-C/client.c
@@ -1,51 +1,95 @@
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <stdio.h>
 #include <netinet/in.h>
-#include <netinet/ip.h>
-#include <string.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-       int x = 8080;
-       if (argc > 1) {
-           x = atoi(argv[1]);
-       }
-       int c;
-       struct sockaddr_in server;
+typedef struct sockaddr_in sockaddr_in;
 
-       c = socket(AF_INET, SOCK_STREAM, 0);
-       if (c < 0) {
-              printf("Socker build failed.\n");
-              return 1;
-       }
+void handleError(char* message, int status) {
+    perror(message);
+    exit(status);
+}
 
-       memset(&server, 0, sizeof(server));
-       server.sin_port = htons(x);
-       server.sin_family = AF_INET;
-       server.sin_addr.s_addr = inet_addr("127.0.0.1");
+int socketInit() {
+    int descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    return descriptor;
+}
 
-       if (connect(c, (struct sockaddr *) &server, sizeof(server)) < 0) {
-              printf("Connection error.\n");
-              return 1;
-       }
+int assignNameToSocket(int socketDescriptor) {
+    sockaddr_in name;
+    name.sin_port        = htons(8080);
+    name.sin_family      = AF_INET;
+    name.sin_addr.s_addr = htons(INADDR_ANY);
 
-       char message[100];
-       printf("message: ");
-       fgets(message, 100, stdin);
+    socklen_t nameSize = sizeof(name);
+    int bindResult = bind(socketDescriptor, (const struct sockaddr*) &name, nameSize);
+    return bindResult;
+}
 
-       uint16_t message_size = strlen(message);
-       uint16_t size = htons(message_size);
-       send(c, &size, sizeof(size), 0);
+int passiveSocket(){
+    // Create a passive socket to listen for connections
+    int socketDescriptor = socketInit();
+    if (socketDescriptor < 0){
+        handleError("SOCKET_ERROR\n An error occurred while creating the socket", 1);
+    }
 
-       for(int i = 0; i < message_size; i++) {
-           send(c, &message[i], sizeof(char), 0);
-           printf("%c", message[i]);
-       }
+    int bindResult = assignNameToSocket(socketDescriptor);
+    if (bindResult < 0){
+        handleError("ASSIGN_NAME_TO_SOCKET\n An error occurred during the binding process. This program failed to assign a name to the created socket.", 2);
+    }
 
-       uint16_t spaces = 0;
-       spaces = ntohs(spaces);
-       recv(c, &spaces, sizeof(spaces), 0);
+    return socketDescriptor;
+}
 
-       printf("Number of spaces %hu\n", spaces);
-       close(c);
+void nameInfo(char descriptiveMessage[], sockaddr_in* name) {
+    char ip[INET_ADDRSTRLEN];
+
+    struct in_addr ipAddr = name->sin_addr;
+    inet_ntop(AF_INET, &ipAddr, ip, INET_ADDRSTRLEN);
+
+    printf("%s -- %s:hu\n", descriptiveMessage, ip, ntohs(name->sin_port));
+}
+
+int acceptConn(int serverSocketDescriptor) {
+    struct sockaddr clientName;
+    socklen_t clientNameSize = sizeof(clientName);
+
+    // Create an active socket with the same information as the passive socket
+    // in order to send and receive data.
+    // Check out the README.md file!
+    int clientSocketDescriptor = accept(
+                                        serverSocketDescriptor,
+                                        &clientName,
+                                        &clientNameSize
+                                        );
+    if(clientSocketDescriptor < 0) {
+      handleError("Cannot accept connection.", 3);
+    }
+
+    // We are costing the sockaddr structure to sockaddr_in structure in order to
+    // get the sin_addr and the other fields, otherwise everything except the
+    // sin_addr will be inside a char[14].
+    nameInfo("Client", (sockaddr_in*) &clientName);
+    return clientSocketDescriptor;
+}
+
+int main() {
+
+    int thisSocketDescriptor = passiveSocket();
+    listen(thisSocketDescriptor, 5);
+
+    int clientSocketDescriptor;
+    printf("Server waiting for data on port 8080: \n");
+    while(1) {
+
+        clientSocketDescriptor = acceptConn(thisSocketDescriptor);
+
+        close(clientSocketDescriptor);
+    }
+
+    return 0;
 }
