4c8412922ca6f92f5261278cfa2f49e6849af5fd
Alexandru Darie <alexdarieph@gmail.com>
2018-11-05 14:30:19 +0200

    Create headers.c

---
+++ utils/headers.c
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
