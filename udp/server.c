ccae8dcf75a16785d271f5f4b7a7101fb4ae9c59
Alexandru Darie <alexdarieph@gmail.com>
2018-11-05 14:29:15 +0200

    Create server.c

---
+++ udp/server.c
@@ -0,0 +1,31 @@
+#include "../utils/headers.c"
+
+void handleError(char message[], int status) {
+    perror(message);
+    exit(status);
+}
+
+int main() {
+    int socketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    if(socketDescriptor < 0) {
+        handleError("socket", 1);
+    }
+
+    struct sockaddr_in serverName;
+    serverName.sin_addr.s_addr = INADDR_ANY;
+    serverName.sin_port = htons(8080);
+    serverName.sin_family = AF_INET;
+
+    int status = bind(socketDescriptor, (struct sockaddr*) &serverName, sizeof(serverName));
+    if(status < 0) {
+        handleError("bind", 3);
+    }
+
+    char message[1024];
+    struct sockaddr_in destinationName;
+    socklen_t destinationSize = sizeof(destinationName);
+    int bytesReceived = recvfrom(socketDescriptor, message, sizeof(message), 0, (struct sockaddr*) &destinationName, &destinationSize);
+    printf("bytes recevied: %d\n", bytesReceived);
+
+    return 0;
+}
